e94c1b2aa3259bf8171106c121d4328df841a820
Ovadya33 <timurgts2710@gmail.com>
2022-12-05 19:17:32 +0300

    fixHomeWork

--- src/telran/shapes/SquareTriangle.java
+++ src/telran/shapes/SquareTriangle.java
@@ -13,14 +13,19 @@ public class SquareTriangle extends Square {
 		String res[] = new String[super.getWidth()];			
 		int lastLine = super.getHeight() - 1;
 		res[lastLine] = super.getLine(offset);
+		
+		if(isLeftDiagonal) {
+			res[0] = " ".repeat(offset) + super.getSymbol();
+		} else {
+			res[0] = " ".repeat(offset + super.getWidth() - 1) + super.getSymbol();
+		}
+		
 		for (int i = 1; i < lastLine; i++) {
 			String end = super.getSymbol() +  super.getOffset(i - 1) + super.getSymbol();
-			if (isLeftDiagonal) {
-				res[0] = " ".repeat(offset) + super.getSymbol();
+			if (isLeftDiagonal) {				
 				res[i] = super.getOffset(offset) + end ;
-			} else {
-				res[0] = " ".repeat(offset + super.getWidth() - 1) + super.getSymbol();
-				res[i] = super.getOffset(offset + super.getWidth() - (1 + i)) + end;
+			} else {				
+				res[i] = super.getOffset(offset + super.getWidth() - (i + 1)) + end;
 			}			
 		}
 		return res;
