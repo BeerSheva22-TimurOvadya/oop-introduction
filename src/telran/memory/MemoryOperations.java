a9eec218b5162c4d4dfc0a39063359cd6df307fb
Ovadya33 <timurgts2710@gmail.com>
2022-12-17 16:28:38 +0300

    FixHomeWork

--- src/telran/memory/MemoryOperations.java
+++ src/telran/memory/MemoryOperations.java
@@ -4,17 +4,17 @@ public class MemoryOperations {
 			public static int getMaxAvailableMemory() {
 				int left = 0;
 				int right = Integer.MAX_VALUE;
-				int res = left + (right - left) / 2;
+				int res = (right + left) / 2;
 				byte[] ar = null;
-				while (left < right) {
+				while (left <= right) {
+					ar = null;
 					try {
 						ar = new byte[res];
 						left = res + 1;
 					} catch (Throwable e) {
 						right = res - 1;
-					}
-					ar = null;
-					res = left + (right - left) / 2;
+					}					
+					res =  (right + left) / 2;
 				} 
 				return res;
 			}
